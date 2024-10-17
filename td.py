39ca2a0f3b11bfd2a55ee643872f9f4b65088ed3
Mohammed Yassin <yssnmed@gmail.com>
2024-10-17 14:52:14 +0100

    Td

---
+++ td.py
@@ -0,0 +1,22 @@
+# Exercice 1 
+semaine = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","samedi","Dimache"]
+print ("",semaine[0:5]) 
+print("",semaine[5:])
+print("Dernier jour de la semaine : ",semaine[6:])
+print("Dernier jour de la semaine : ",semaine[-1:])
+print("Inverser : ",semaine[ ::-1 ] )
+# Exercice 2 
+Hiver=["decembre","janvier","fevrier"]
+Printemps = ["Mars","April ", "May"]
+ete = ["juin","juillet","Aout"]
+Automne = ["septembre" , "octobre ", "novembre"]
+saisons  = [Automne , Hiver , Printemps , ete ]
+print("Saisons : ", saisons[:][1])
+# Exercice 3
+list = list(range(0,9))
+x = range(11)
+for n in x:
+  print(n * 9)
+# list = list(range(0,91,9))
+
+
